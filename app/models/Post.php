b79d204996e56df4593671ced9da54b98956b066
timdanielscode <timdanielscode@hotmail.com>
2022-03-03 13:10:53 +0100

    Added Post model
    Signed-off-by: timdanielscode <timdanielscode@hotmail.com>

---
+++ app/models/Post.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\models;
+
+class Post {
+
+    public $t = "posts",
+
+        $id = 'id', 
+        $title = 'title', 
+        $body = 'body', 
+        $created_at = 'created_at', 
+        $updated_at = 'updated_at';
+}
