aa777d8eee828ebab747df6a4285348eb2f584d3
timdanielscode <timdanielscode@hotmail.com>
2022-03-03 12:40:31 +0100

    Added style to tables and index view
    Signed-off-by: timdanielscode <timdanielscode@hotmail.com>

--- app/views/admin/users/index.php
+++ app/views/admin/users/index.php
@@ -8,35 +8,34 @@
     $this->include('header');
     $this->include('navbar');
 ?>
-<div class="container">
+<div class="con">
 
     <?php if(parts\Session::exists("updated")) { ?>
-        <div class="my-3"><?php echo parts\Alert::display("success", "updated"); ?></div>
+        <div class=""><?php echo parts\Alert::display("success", "updated"); ?></div>
     <?php parts\Session::delete('updated');  } ?>
 
     <?php if(Session::exists("registered")) { ?>
-        <div class="my-5 w-75 mx-auto"><?php echo Alert::display("warning", "registered"); ?></div>
+        <div class=""><?php echo Alert::display("warning", "registered"); ?></div>
     <?php Session::delete('registered'); } ?>
 
-    <a class="btn bg-color-sec text-white" href="/admin/users/create">Add user</a>
-    <form action="" method="GET">
-        <div class="form-row mt-5">
-            <div class="form-group">
-                <input type="text" name="search" class="form-control" placeholder="Search">
-            </div>
+    <a class="button margin-t-50" href="/admin/users/create">Add user</a>
+
+    <form action="" method="GET" class="margin-t-50">
+        <div class="form-parts">
+            <input type="text" name="search" class="search" placeholder="Search">
         </div>
     </form>
-    <table class="table table-striped mt-5">
+    <table>
         
             <thead>
                 <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Username</th>
-                    <th scope="col">Email</th>
-                    <th scope="col">Role</th>
-                    <th scope="col">Show</th>
-                    <th scope="col">Edit</th>
-                    <th scope="col">Delete</th>
+                    <th>#</th>
+                    <th>Username</th>
+                    <th>Email</th>
+                    <th>Role</th>
+                    <th>Show</th>
+                    <th>Edit</th>
+                    <th>Delete</th>
                 </tr>
             </thead>
             <tbody>
@@ -55,13 +54,13 @@
                             <?php echo $user['name']; ?>
                         </td>
                         <td>
-                            <a class="btn bg-color-sec text-white" href="/admin/users/<?php echo $user["id"]; ?>/username/<?php echo $user["username"]; ?>">Read</a>
+                            <a href="/admin/users/<?php echo $user["id"]; ?>/username/<?php echo $user["username"]; ?>">Read</a>
                         </td>
                         <td>
-                            <a class="btn bg-color-sec text-white" href="/admin/users/<?php echo $user["id"]; ?>/username/<?php echo $user["username"]; ?>/edit">Edit</a>
+                            <a href="/admin/users/<?php echo $user["id"]; ?>/username/<?php echo $user["username"]; ?>/edit">Edit</a>
                         </td>   
                         <td>
-                            <a class="btn bg-danger text-white" href="/admin/users/<?php echo $user["id"]; ?>/username/<?php echo $user["username"]; ?>/delete" onclick="return confirm('Are you sure you want to delete this?');">Delete</a>
+                            <a href="/admin/users/<?php echo $user["id"]; ?>/username/<?php echo $user["username"]; ?>/delete" onclick="return confirm('Are you sure you want to delete this?');">Delete</a>
                         </td>
                     </tr>
                 <?php } ?>
