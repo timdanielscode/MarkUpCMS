3529fe0ee45b47eb75261ab497b21de5497352a2
timdanielscode <timdanielscode@hotmail.com>
2022-03-10 17:59:31 +0100

    Added working p tag button
    Signed-off-by: timdanielscode <timdanielscode@hotmail.com>

--- pub/assets/js/wysiwyg.js
+++ pub/assets/js/wysiwyg.js
@@ -1,17 +1,24 @@
 var selected = "";
+var startSelection = "";
 
 document.addEventListener("selectionchange", e => {
     selection = document.getSelection().toString();
     selected = selection;
-})
+});
 
 var pTag = document.getElementById("pTag");
 
 pTag.addEventListener('click', clickHandler);
 
 function clickHandler() {
-    console.log(selected);
+
+    var startP = "<p>";
+    var endP = "</p>";
+    var addedP = startP + selected + endP;
+    var body = document.getElementById("body");
+    body.value = body.value.replace(selection,addedP);
 }
+
 
 
    
