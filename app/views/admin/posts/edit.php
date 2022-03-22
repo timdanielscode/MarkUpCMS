f3353d5edbd4f3f3ae5409b03c41bd18b4a98db9
timdanielscode <timdanielscode@hotmail.com>
2022-03-22 14:44:42 +0100

    Added tinyMCE to the project
    Signed-off-by: timdanielscode <timdanielscode@hotmail.com>

--- app/views/admin/posts/edit.php
+++ app/views/admin/posts/edit.php
@@ -15,54 +15,32 @@
 
 
     <div class="row">
-    <div class="col10">
-
-    <form action="" method="POST">
-        <div class="form-parts">
-            <input name="title" id="title" value="<?php echo $post['title']; ?>">
-            <div class="error-messages">
-                <?php echo Errors::get($rules, 'title'); ?>
+        <div class="col10">
+            <form action="" method="POST">
+                <div class="form-parts">
+                    <input name="title" id="title" value="<?php echo $post['title']; ?>">
+                    <div class="error-messages">
+                        <?php echo Errors::get($rules, 'title'); ?>
+                    </div>
+                </div>
+                <textarea name="body" id="body"><?php echo htmlspecialchars_decode($post['body']); ?></textarea>
+                <button name="submit" id="submit" type="submit" class="display-none">Create</button>
+                <input type="hidden" name="token" value="<?php Csrf::token('add');?>" />
+            </form>
+        </div>
+        <div class="col2">
+            <div id="postSidebar">
+                <div class="row">
+                    <div class="col6">
+                        <a href="/admin/posts" class="button margin-t-50">Back</a>
+                    </div>
+                    <div class="col6">
+                        <label for="submit" class="button margin-t-50">Submit</label>
+                    </div>
+                </div>
             </div>
         </div>
-
-        
-        <textarea name="body" id="body"><?php echo htmlspecialchars_decode($post['body']); ?></textarea>
-
-        <button name="submit" id="submit" type="submit" class="display-none">Create</button>
-        <input type="hidden" name="token" value="<?php Csrf::token('add');?>" />
-    </form>
-</div>
-<div class="col2">
-    <div id="postSidebar">
-    <div class="row">
-        <div class="col6">
-            <a href="/admin/posts" class="button margin-t-50">Back</a>
-        </div>  
-        <div class="col6">
-            <label for="submit" class="button margin-t-50">Submit</label>
-        </div>
     </div>
-    <div class="row">
-        <div class="col6">
-            <button onclick="toVisualBuilder();" class="button margin-t-50">Visual Builder</button>
-        </div>  
-        <div class="col6">
-            <button  onclick="toBuilder();" class="button margin-t-50">Builder</button>
-        </div>
-    </div>
-
-
-        <div id="wysiwg" class="margin-t-50">
-            <button onclick="clickHandler('p')" id="pTag" value="p">p</button>
-            <button onclick="clickHandler('div')" id="pTag" value="div">div</button>
-            <button onclick="clickHandler('h1')" id="pTag" value="h1">h1</button>
-            <button onclick="clickHandler('h2')" id="pTag" value="h2">h2</button>
-            <button onclick="clickHandler('h3')" id="pTag" value="h3">h3</button>
-        </div>
-  
-    </div>
-</div>
-</div>
 
 <?php 
     $this->include('footer');
--- app/views/includes/header.php
+++ app/views/includes/header.php
@@ -13,4 +13,4 @@ foreach($files as $file) {
         echo '<script defer src="/assets/js/' . $file . '"></script>';
     }
 }
-echo '</head><body>';
+echo '<script src="https://cdn.tiny.cloud/1/yjrgki0oubi33qi9ebe57t1lz8lw9nbe3xbnfrv5893n4oqb/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script></head><body>';
