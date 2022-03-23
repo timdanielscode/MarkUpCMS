48b4995710cb93d4c1d22dcc8f44cfa3183f50f6
timdanielscode <timdanielscode@hotmail.com>
2022-03-23 12:10:18 +0100

    Added body tags to render post page
    Signed-off-by: timdanielscode <timdanielscode@hotmail.com>

--- app/views/admin/posts/page.php
+++ app/views/admin/posts/page.php
@@ -5,5 +5,6 @@
     <?php if(!empty($post['metaDescription']) ) {echo '<meta name="description" content="'.$post['metaDescription'].'">'; } ?>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
-
-<?php echo html_entity_decode($post["body"]); ?>
+<body>
+    <?php echo html_entity_decode($post["body"]); ?>
+</body>
