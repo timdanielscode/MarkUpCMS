9322fe45e070073a922833244511c34006d85c5e
timdanielscode <timdanielscode@hotmail.com>
2022-03-23 12:09:22 +0100

    Added meta data to render post page
    Signed-off-by: timdanielscode <timdanielscode@hotmail.com>

--- app/views/admin/posts/page.php
+++ app/views/admin/posts/page.php
@@ -1,1 +1,9 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <?php if(!empty($post['metaTitle']) ) {echo "<title>".$post['metaTitle']."</title>"; } ?>
+    <?php if(!empty($post['metaDescription']) ) {echo '<meta name="description" content="'.$post['metaDescription'].'">'; } ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
 <?php echo html_entity_decode($post["body"]); ?>
