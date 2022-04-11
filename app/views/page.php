a0b08abbc1aa55803167f9a2d111e97557bb28ac
timdanielscode <timdanielscode@hotmail.com>
2022-04-11 11:00:57 +0200

    Loading css and js files on page bug fix
    Signed-off-by: timdanielscode <timdanielscode@hotmail.com>

--- app/views/page.php
+++ app/views/page.php
@@ -6,12 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php foreach($cssFiles as $cssFile) {
         if (file_exists('website/assets/css/' . $cssFile['file_name'].$cssFile['extension'])) {
-            echo '<link rel="stylesheet" type="text/css" href="website/assets/css/' . $cssFile['file_name'].$cssFile['extension'] . '">';
+            echo '<link rel="stylesheet" type="text/css" href="/website/assets/css/' . $cssFile['file_name'].$cssFile['extension'] . '">';
         } 
     } ?>
     <?php foreach($jsFiles as $jsFile) {
         if (file_exists('website/assets/js/' . $jsFile['file_name'].$jsFile['extension'])) {
-            echo '<script src="website/assets/js/' . $jsFile['file_name'].$jsFile['extension'] . '"></script>';
+            echo '<script src="/website/assets/js/' . $jsFile['file_name'].$jsFile['extension'] . '"></script>';
         } 
     } ?>
 </head>
