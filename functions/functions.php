3da49e100d53de3cfb9029f3f0a3e98ab8919c42
timdanielscode <timdanielscode@hotmail.com>
2023-03-18 20:13:30 +0000

    Update framework functions.php

--- functions/functions.php
+++ functions/functions.php
@@ -1,18 +1,25 @@
 <?php
 /**
+ * Functions
+ * 
  * @author Tim Daniëls
- * @version 1.0
  */
 
+/** 
+ * @param string $path header
+ * @return void
+ */     
 function redirect($path) {
     
     if($path) {
         header('location: '.$path);
-    } else {
-        echo "Path not found!";
-    }
+    } 
 }
 
+/** 
+ * @param string $name _POST|_GET
+ * @return bool
+ */     
 function submitted($name) {
 
     if(!empty($name) && isset($_POST[$name])) {
@@ -24,29 +31,38 @@ function submitted($name) {
     }
 }
 
+/** 
+ * @param string $name optional
+ * @return global _POST
+ */  
 function post($name = null) {
 
     if(!empty($name) && isset($_POST[$name])) {
-        //$post = htmlspecialchars($_POST[$name]);
-        $post = $_POST[$name];
+
+        $post = htmlspecialchars($_POST[$name]);
         return $post;
     } 
 }
 
+/** 
+ * @param string $name optional
+ * @return global _GET
+ */  
 function get($name) {
 
     if(!empty($name) && isset($_GET[$name])) {
+
         $get = htmlentities($_GET[$name], ENT_QUOTES, 'UTF-8');
         return $get;
     } 
 }
 
+/** 
+ * @param string $path file
+ * @return void
+ */
 function involve($path) {
 
     require_once $path;
-}
-
-function loadImage($path) {
-    echo $path;
 }
 
