560e4f9ddcfa170acad7679fd3a70abd47d5f6c0
timdanielscode <timdanielscode@hotmail.com>
2023-03-21 22:08:08 +0000

    Removed html from page.php

--- app/views/page.php
+++ app/views/page.php
@@ -1,43 +1,21 @@
-<!DOCTYPE html>
-<head>
-    <meta charset="UTF-8">
-    <?php if(!empty($post['metaTitle']) ) {echo "<title>".$post['metaTitle']."</title>"; } ?>
-    <?php if(!empty($post['metaDescription']) ) {echo '<meta name="description" content="'.$post['metaDescription'].'">'; } ?>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php foreach($cssFiles as $cssFile) {
-        if (file_exists('website/assets/css/' . $cssFile['file_name'].$cssFile['extension'])) {
-            echo '<link rel="stylesheet" type="text/css" href="website/assets/css/' . $cssFile['file_name'].$cssFile['extension'] . '">';
-        } 
-    } ?>
-    <?php foreach($jsFiles as $jsFile) {
-        if (file_exists('website/assets/js/' . $jsFile['file_name'].$jsFile['extension'])) {
-            echo '<script src="website/assets/js/' . $jsFile['file_name'].$jsFile['extension'] . '"></script>';
-        } 
-    } ?>
-</head>
-<body>
-    <?php 
+<?php 
     
-        if(!empty($menusTop ) ) { 
+    if(!empty($menusTop ) ) { 
 
-            foreach($menusTop as $menuTop) {
+        foreach($menusTop as $menuTop) {
 
-                echo html_entity_decode($menuTop['content']); 
-            }
-
+            echo html_entity_decode($menuTop['content']); 
         }
 
-        echo html_entity_decode($post["body"]); 
+    }
+
+    echo html_entity_decode($post["body"]); 
      
-        if(!empty($menusBottom ) ) { 
+    if(!empty($menusBottom ) ) { 
 
-            foreach($menusBottom as $menuBottom) {
-                echo html_entity_decode($menuBottom['content']); 
-            }
-
+        foreach($menusBottom as $menuBottom) {
+            echo html_entity_decode($menuBottom['content']); 
         }
 
+    }
 
-    ?>
-
-</body>
