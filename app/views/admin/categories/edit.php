10746a5ff23b3a4db4dc48c8529ebd7fb968dff6
timdanielscode <timdanielscode@hotmail.com>
2023-07-13 18:47:45 +0000

    Added the actual update button back for updating the category title and description

--- app/views/admin/categories/edit.php
+++ app/views/admin/categories/edit.php
@@ -2,3 +2,5 @@
 <textarea id="DESCRIPTION" type="text"><?php echo $description; ?></textarea>
 <input type="hidden" id="ID" value="<?php echo $id; ?>">
 <div class="MESSAGE"></div>
+
+<button class="" id="UPDATE">Update</button>
