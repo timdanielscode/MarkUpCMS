681c01a374319936835bdfb62a8cc0234130182a
timdanielscode <timdanielscode@hotmail.com>
2023-07-23 08:14:32 +0000

    Added counters on dashboard sidebar

--- app/controllers/admin/DashboardController.php
+++ app/controllers/admin/DashboardController.php
@@ -1,13 +1,22 @@
 <?php
 
-  namespace app\controllers\admin;
+namespace app\controllers\admin;
 
-  use app\controllers\Controller;
-                
-  class DashboardController extends Controller {
+use app\controllers\Controller;
+use database\DB;
+                    
+class DashboardController extends Controller {
                 
     public function index() {    
 
-        return $this->view("admin/dashboard/index");     
+        $data['pages'] = DB::try()->select('id')->from('pages')->fetch();
+        $data['menus'] = DB::try()->select('id')->from('menus')->fetch();
+        $data['categories'] = DB::try()->select('id')->from('categories')->fetch();
+        $data['css'] = DB::try()->select('id')->from('css')->fetch();
+        $data['js'] = DB::try()->select('id')->from('js')->fetch();
+        $data['media'] = DB::try()->select('id')->from('media')->fetch();
+        $data['users'] = DB::try()->select('id')->from('users')->fetch();
+
+        return $this->view("admin/dashboard/index", $data);     
     }
 }  
--- app/views/admin/dashboard/index.php
+++ app/views/admin/dashboard/index.php
@@ -22,37 +22,37 @@
                 <div class="sidebarContainer">
                     <nav id="navigationMenu">
                         <ul id="dropdownItems">
-                            <li class="dropdownItem">Pages</li>
+                            <li class="dropdownItem"></span>Pages<span class="count"><?php echo count($pages); ?></li>
                             <ul class="dropdown display-none">
                                 <li><a href="/admin/posts">Table</a></li>
                                 <li><a href="/admin/posts/create">New page</a></li>
                             </ul>
-                            <li class="dropdownItem">Menus</li>
+                            <li class="dropdownItem">Menus<span class="count"><?php echo count($menus); ?></span></li>
                             <ul class="dropdown display-none">
                                 <li><a href="/admin/menus">Table</a></li>
                                 <li><a href="/admin/menus/create">New menu</a></li>
                             </ul>
-                            <li class="dropdownItem">Categories</li>
+                            <li class="dropdownItem">Categories<span class="count"><?php echo count($categories); ?></span></li>
                             <ul class="dropdown display-none">
                                 <li><a href="/admin/categories">Table</a></li>
                                 <li><a href="/admin/categories/create">New category</a></li>
                             </ul>
-                            <li class="dropdownItem">Css</li>
+                            <li class="dropdownItem">Css<span class="count"><?php echo count($css); ?></span></li>
                             <ul class="dropdown display-none">
                                 <li><a href="/admin/css">Table</a></li>
                                 <li><a href="/admin/css/create">New stylesheet</a></li>
                             </ul>
-                            <li class="dropdownItem">Js</li>
+                            <li class="dropdownItem">Js<span class="count"><?php echo count($js); ?></span></li>
                             <ul class="dropdown display-none">
                                 <li><a href="/admin/js">Table</a></li>
                                 <li><a href="/admin/js/create">New script</a></li>
                             </ul>
-                            <li class="dropdownItem">Media</a></li>
+                            <li class="dropdownItem">Media<span class="count"><?php echo count($media); ?></span></a></li>
                             <ul class="dropdown display-none">
                                 <li><a href="/admin/media">Table</a></li>
                                 <li><a href="/admin/media/create">Upload files</a></li>
                             </ul>
-                            <li class="dropdownItem">Users</a></li>
+                            <li class="dropdownItem">Users<span class="count"><?php echo count($users); ?></span></a></li>
                             <ul class="dropdown display-none">
                                 <li><a href="/admin/users">Table</a></li>
                                 <li><a href="/admin/users/create">New user</a></li>
