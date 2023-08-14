3f689546c3de54f84614139f48362fe550f063cf
timdanielscode <timdanielscode@hotmail.com>
2023-08-14 14:03:50 +0000

    Update media index view and media index delete js file

--- pub/assets/js/media/index/delete.js
+++ pub/assets/js/media/index/delete.js
@@ -4,6 +4,9 @@ $(document).ready(function() {
 
     $(document).on('click', '.deleteCheckbox', function() {
 
+        document.querySelector('.indexDeleteForm').classList.remove('display-none-important')
+        document.querySelector('.deleteSeparator').classList.remove('display-none')
+
         if(jQuery.inArray(this.value, collection) !== -1) {
 
             collection.splice($.inArray(this.value, collection), 1);
