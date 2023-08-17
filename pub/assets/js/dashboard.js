bcbec2030c039efada79d2bdbe29e6112d1d8c79
timdanielscode <timdanielscode@hotmail.com>
2023-08-17 12:08:49 +0000

    Update dashboard js file

--- pub/assets/js/dashboard.js
+++ pub/assets/js/dashboard.js
@@ -1,23 +1,22 @@
-var navigationMenu = document.getElementById('navigationMenu');
-var dropdownItems = navigationMenu.children[0];
+var dropdownItems = document.querySelectorAll('.dropdownItem');
 
-for(var dropdownItem of dropdownItems.children) {
+for(var dropdownItem of dropdownItems) {
 
     if(dropdownItem.classList.contains('dropdown') === false) {
 
         dropdownItem.addEventListener('click', function() { 
-            
-            clearOpenDropdownMenu(dropdownItem.parentNode.children, this.nextElementSibling);
-            this.nextElementSibling.classList.toggle('display-none');
+                
+            clearOpenDropdownMenu(this, dropdownItem.parentNode.children, this.nextElementSibling);
+            this.nextElementSibling.classList.toggle('display-none')
         });
     }
 }
 
-function clearOpenDropdownMenu(elements, justOpenedElement) {
+function clearOpenDropdownMenu(dropdownItem, elements, justOpenedElement) {
 
     for(var element of elements) {
 
-        if(element.classList.contains('dropdown') === true && element.classList.contains('display-none') === false && element !== justOpenedElement) {
+        if(element.classList.contains('dropdown') === true && element.classList.contains('display-none') === false && element !== justOpenedElement && dropdownItem.parentNode.classList.contains('dropdown') === false) {
 
             element.classList.add('display-none')
         }
