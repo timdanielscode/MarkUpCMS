661ac9810a6807621bf4db5b7f524c0b8839e1d5
timdanielscode <timdanielscode@hotmail.com>
2023-09-04 18:54:30 +0000

    Update users create view

--- app/views/admin/users/create.php
+++ app/views/admin/users/create.php
@@ -14,67 +14,67 @@
     $this->include('navbar');
 ?>
 
-<div class="create-container">
-    <h1 class="mb-5">Add a new user</h1>
     <div class="row">
         <div class="col10 col9-L">
-            <form action="store" method="POST" class="usersCreateForm">
-            <div class="form-parts">
-                    <label for="username">Username:</label>
-                    <input name="f_username" type="username" id="username" value="<?php echo post('f_username'); ?>">
-                    <div class="error-messages">
-                        <?php echo Errors::get($rules, 'f_username'); ?>
+            <div class="create-container">
+            <h1 class="mb-5">Add a new user</h1>
+                <form action="store" method="POST" class="usersCreateForm">
+                    <div class="form-parts">
+                        <label for="username">Username:</label>
+                        <input name="f_username" type="username" id="username" value="<?php echo post('f_username'); ?>">
+                        <div class="error-messages">
+                            <?php echo Errors::get($rules, 'f_username'); ?>
+                        </div>
                     </div>
-                </div>
-                <div class="form-parts">
-                    <label for="email">Email:</label>
-                    <input name="email" type="email" id="email" value="<?php echo post('email'); ?>">
-                    <div class="error-messages">
-                        <?php echo Errors::get($rules, 'email'); ?>
+                    <div class="form-parts">
+                        <label for="email">Email:</label>
+                        <input name="email" type="email" id="email" value="<?php echo post('email'); ?>">
+                        <div class="error-messages">
+                            <?php echo Errors::get($rules, 'email'); ?>
+                        </div>
                     </div>
-                </div>
-                <div class="form-parts">
-                    <label for="password">Password:</label>
-                    <input name="password" type="password" id="password" value="<?php echo post('password'); ?>">
-                    <div class="error-messages">
-                        <?php echo Errors::get($rules, 'password'); ?>
+                    <div class="form-parts">
+                        <label for="password">Password:</label>
+                        <input name="password" type="password" id="password" value="<?php echo post('password'); ?>">
+                        <div class="error-messages">
+                            <?php echo Errors::get($rules, 'password'); ?>
+                        </div>
                     </div>
-                </div>
-                <div class="form-parts">
-                    <label for="password_confirm">Retype password:</label>
-                    <input name="password_confirm" type="password" id="password_confirm" value="<?php echo post('password_confirm'); ?>">
-                    <div class="error-messages">
-                        <?php echo Errors::get($rules, 'password_confirm'); ?>
+                    <div class="form-parts">
+                        <label for="password_confirm">Retype password:</label>
+                        <input name="password_confirm" type="password" id="password_confirm" value="<?php echo post('password_confirm'); ?>">
+                        <div class="error-messages">
+                            <?php echo Errors::get($rules, 'password_confirm'); ?>
+                        </div>
                     </div>
-                </div>
-                <div class="form-parts">
-                    <label for="role">Role:</label>
-                    <select name="role" id="role">
-                        <option>Normal</option>
-                        <option>Admin</option>
-                    </select>
-                    <div class="error-messages">
-                        <?php echo Errors::get($rules, 'role'); ?>
+                    <div class="form-parts">
+                        <label for="role">Role:</label>
+                        <select name="role" id="role">
+                            <option>Normal</option>
+                            <option>Admin</option>
+                        </select>
+                        <div class="error-messages">
+                            <?php echo Errors::get($rules, 'role'); ?>
+                        </div>
                     </div>
-                </div>
-                <div class="form-parts">
-                    <button id="submit" name="submit" type="submit" class="display-none"></button>
-                    <input type="hidden" name="token" value="<?php Csrf::token('add');?>" />
-                </div>
-            </form>
+                    <div class="form-parts">
+                        <button id="submit" name="submit" type="submit" class="display-none"></button>
+                        <input type="hidden" name="token" value="<?php Csrf::token('add');?>" />
+                    </div>
+                </form>
+            </div>
         </div>
         <div class="col2 col3-L">
             <div id="sidebar" class="width-25-L">
                 <div class="sidebarContainer">
                     <div class="mainButtonContainer">
-                        <label for="submit" class="button">Store</label>
-                        <a href="/admin/users" class="button">Back</a>
+                        <label for="submit" class="button greenButton margin-r-10">Store</label>
+                        <a href="/admin/users" class="button blueButton">Back</a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
 <?php 
     $this->include('footer');
