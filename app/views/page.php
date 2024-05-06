ad216e9b05f29edf8218e41e5af3a2ea1a869c79
timdanielscode <timdanielscode@hotmail.com>
2024-05-06 10:20:17 +0000

    Bug fix page view

--- app/views/page.php
+++ app/views/page.php
@@ -3,9 +3,9 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width,initial-scale=1">
-        <?php if(!empty($page['metaTitle']) && $page['metaTitle'] !== null) { ?><title><?php echo $page['metaTitle']; ?></title><?php } ?>
-        <?php if(!empty($page['metaDescription']) && $page['metaDescription'] !== null) { ?><meta name="description" content="<?php echo $page['metaDescription']; ?>"/><?php } ?>
-        <?php if(!empty($page['metaKeywords']) && $page['metaKeywords'] !== null) { ?><meta name="keywords" content="<?php echo $page['metaKeywords']; ?>"/><?php } ?>
+        <?php if(!empty($page[0]['metaTitle']) && $page[0]['metaTitle'] !== null) { ?><title><?php echo $page[0]['metaTitle']; ?></title><?php } ?>
+        <?php if(!empty($page[0]['metaDescription']) && $page[0]['metaDescription'] !== null) { ?><meta name="description" content="<?php echo $page[0]['metaDescription']; ?>"/><?php } ?>
+        <?php if(!empty($page[0]['metaKeywords']) && $page[0]['metaKeywords'] !== null) { ?><meta name="keywords" content="<?php echo $page[0]['metaKeywords']; ?>"/><?php } ?>
 <?php if(!empty($metas) && $metas !== null) { foreach($metas as $meta) { echo htmlspecialchars_decode($meta['content']); } } ?>
 <?php if(!empty($cssFiles) && $cssFiles !== null) { ?><?php foreach($cssFiles as $cssFile) { ?><link rel="stylesheet" href="/website/assets/css/<?php echo $cssFile['file_name'] . $cssFile['extension']; ?>"><?php } ?><?php } ?>
 <?php if(!empty($jsFiles) && $jsFiles !== null) { ?><?php foreach($jsFiles as $jsFile) { ?><script type="text/javascript" src="/website/assets/js/<?php echo $jsFile['file_name'] . $jsFile['extension']; ?>" defer></script><?php } ?><?php } ?>
